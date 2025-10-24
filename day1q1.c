8ddd51f68a03cafa01c19decf3d1cea696da6782
pavanijain <pavanijain12@gmail.com>
2025-10-24 19:18:25 +0530

    day1q1.c

---
+++ day1q1.c
@@ -0,0 +1,11 @@
+//Q1: Write a program to input two numbers and display their sum
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter two integers: ");
+    scanf("%d%d",&a,&b);
+    printf("Sum = %d\n",a+b);
+    return 0;
+}
